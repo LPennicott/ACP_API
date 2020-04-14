53823b7638906e001592390019932a0dc43de18c
Leighton <LPennicott3@gmail.com>
2020-04-13 22:20:06 -0400

    First Commit
    Slight modification to some lines

--- acpapi.py
+++ acpapi.py
@@ -11,23 +11,23 @@ labels = {
 label_formats = ['PNG', 'PDF', 'EPL2', None]
 
 
-def create_mailing(xml, apikey, test_mode=0, label='None', label_format=None):
-    
-    parameters = {
-                    'apikey': apikey,
-                    'command': 'createMailing',
-                    'testMode': test_mode,
-                    'data': xml,
-                    'label': label,
-                    'labelFormat': label_format,
-                }
-    if label in labels.keys():
-        r = requests.post(url, parameters)
+def create_mailing(xml, apikey, test_mode=1, label=0, label_format=None):
+
+    if label in (*labels.keys(), 0):
+        parameters = {
+                        'apikey': apikey,
+                        'command': 'createMailing',
+                        'testMode': test_mode,
+                        'data': xml,
+                        'label': label,
+                        'labelFormat': label_format,
+                    }
+    r = requests.post(url, parameters)
     return r.content, r.status_code
 
 
 def delete_mailing(apikey, tracking_number, shipperitemid=None):
-    xml = f'''<?xml version="1.0" encoding="utf-8" ?> 
+    xml = f'''<?xml version="1.0" encoding="utf-8" ?>
                 <shippingApiRequest>
                     <mailItem>
                         <trackingNumber>{tracking_number}</trackingNumber>
@@ -45,7 +45,7 @@ def delete_mailing(apikey, tracking_number, shipperitemid=None):
 
 def create_bag(apikey, tracking_number, bag_id=None, comment=None,
     shipper_item_id=None, label=0, label_format='PNG'):
-    xml = f'''<?xml version="1.0" encoding="utf-8" ?> 
+    xml = f'''<?xml version="1.0" encoding="utf-8" ?>
                 <shippingApiRequest>
                     <bag>
                         <shipperBagId>{bag_id}</shipperBagId>
