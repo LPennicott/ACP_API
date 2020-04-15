13b0eb20003e7e1eec310c83556385b230be86bb
Leighton <LPennicott3@gmail.com>
2020-04-15 09:48:16 -0400

    XML Format
    Initial XML format creation for create mailing command

--- mailings.py
+++ mailings.py
@@ -8,16 +8,103 @@ root.append(mailItem)
 # If client uses their own label, they need to provide tracking number.
 if tracking_number:
     trackingNumber = xml.SubElement(mailItem, "trackingNumber")
-    trackingNumber.text = f"{tracking_number}"
+    trackingNumber.text = tracking_number
 
 shipperItemID = xml.SubElement(mailItem, "shipperItemID")
-shipperItemID.text = f"{shipper_item}"
+shipperItemID.text = shipper_item
 displayItemID = xml.SubElement(mailItem, "displayItemID")
-displayItemID.text = f"{display_item}"
+displayItemID.text = display_item
 
 consignee = xml.Element("consignee")
 mailItem.append(consignee)
 name = xml.SubElement(consignee, "name")
-name.text = f"{consignee_name}"
+name.text = consignee_name
 companyname = xml.SubElement(consignee, "companyName")
-companyname.text = f"{consignee_company}"
+companyname.text = consignee_company
+consignee_street = xml.SubElement(consignee, "addressLine1")
+consignee_street.text = con_street_address
+consignee_suite = xml.SubElement(consignee, "addressLine2")
+consignee_suite.text = con_suite
+consignee_city = xml.SubElement(consignee, "city")
+consignee_city.text = con_city
+consignee_state = xml.SubElement(consignee, "state")
+consignee_state.text = con_state
+consignee_zip = xml.SubElement(consignee, "zip")
+consignee_zip.text = con_zip
+consignee_phone = xml.SubElement(consignee, "phone")
+consignee_phone.text = con_phone
+consignee_email = xml.SubElement(consignee, "email")
+consignee_email.text = con_email
+
+shipper = xml.Element("shipper")
+mailItem.append(shipper)
+shipper_name = xml.SubElement(shipper, "name")
+shipper_name.text = shipper_name
+shipper_company_name = xml.SubElement(shipper, "companyName")
+shipper_company_name.text = shipper_company
+shipper_street = xml.SubElement(shipper, "addressLine1")
+shipper_street.text = shipper_street_address
+shipper_suite = xml.SubElement(shipper, "addressLine2")
+shipper_suite.text = shipper_suite
+shipper_city = xml.SubElement(shipper, "city")
+shipper_city.text = shipper_city
+shipper_state = xml.SubElement(shipper, "state")
+shipper_state.text = shipper_state
+shipper_zip = xml.SubElement(shipper, "zip")
+shipper_zip.text = shipper_zip
+shipper_phone = xml.SubElement(shipper, "phone")
+shipper_phone.text = shipper_phone
+shipper_email = xml.SubElement(shipper, "email")
+shipper_email.text = shipper_email
+
+if return_address:
+    return_address = xml.Element("returnAddress")
+    mailItem.append(return_address)
+    return_name = xml.SubElement(return_address, "name")
+    return_name.text = return_name
+    return_company_name = xml.SubElement(return_address, "companyName")
+    return_company_name.text = return_company
+    return_street = xml.SubElement(return_address, "addressLine1")
+    return_street.text = return_street_address
+    return_suite = xml.SubElement(return_address, "addressLine2")
+    return_suite.text = return_suite
+    return_city = xml.SubElement(return_address, "city")
+    return_city.text = return_city
+    return_state = xml.SubElement(return_address, "state")
+    return_state.text = return_state
+    return_zip = xml.SubElement(return_address, "zip")
+    return_zip.text = return_zip
+    return_phone = xml.SubElement(return_address, "phone")
+    return_phone.text = return_phone
+    return_email = xml.SubElement(return_address, "email")
+    return_email.text = return_email  
+
+value = xml.Element("value")
+value.text = parcel_value
+mailItem.append(value)
+
+currency = xml.Element("currency")
+currency.text = parcel_currency
+mailItem.append(currency)
+
+weight = xml.Element("weight")
+weight.text = parcel_weight
+mailItem.append(weight)
+
+weightUOM = xml.Element("weightUnitOfMeasure")
+weightUOM.text = "kg"
+mailItem.append(weightUOM)
+
+dimension = xml.Element("dimensions")
+mailItem.append(dimension)
+parcel_length = xml.SubElement(dimension, "length")
+parcel_length.text = p_length
+width = xml.SubElement(dimension, "width")
+width.text = parcel_width
+height = xml.SubElement(dimension, "height")
+height.text = parcel_height
+
+dimensionsUOM = xml.Element("dimensionsUnitOfMeasure")
+dimensionsUOM.text = "m"
+mailItem.append(dimensionsUOM)
+
