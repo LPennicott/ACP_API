c3c478a55ea4edba6355665d90281ce7aea621e2
Leighton <LPennicott3@gmail.com>
2020-08-03 07:12:25 -0400

    Removed Test
    Removed mailings test if file is run directly.

--- acpapi.py
+++ acpapi.py
@@ -3,58 +3,69 @@ import requests
 import PySimpleGUI as sg
 import mailings
 
-url = 'https://acpapi.com/API/'
+url = "https://acpapi.com/API/"
 labels = {
-    'None': 0,
-    'Ground': 1,
-    'Expedited': 2,
-    'Expedited Max': 4,
-    'Priority': 3,
+    "None": 0,
+    "Ground": 1,
+    "Expedited": 2,
+    "Expedited Max": 4,
+    "Priority": 3,
 }
-label_formats = ['PNG', 'PDF', 'EPL2', ]
+label_formats = [
+    "PNG",
+    "PDF",
+    "EPL2",
+]
 
 
-def create_mailing(csv, apikey, test_mode=1, label='None', label_format='PNG'):
+def create_mailing(csv, apikey, test_mode=1, label="None", label_format="PNG"):
 
     xml = mailings.convert_csv_to_ACPAPI_format(csv)
     # If label_format not valid, default to PNG
     if label_format not in label_formats:
-        label_format = 'PNG'
+        label_format = "PNG"
     # Options for API create mailing command
     parameters = {
-        'apikey': apikey,
-        'command': 'createMailing',
-        'testMode': test_mode,
-        'data': xml,
+        "apikey": apikey,
+        "command": "createMailing",
+        "testMode": test_mode,
+        "data": xml,
         # If label selection not valid, default to no label
-        'label': labels.get(label, 'None'),
-        'labelFormat': label_format,
+        "label": labels.get(label, "None"),
+        "labelFormat": label_format,
     }
     r = requests.post(url, parameters)
     return r.content, r.status_code
 
 
 def delete_mailing(apikey, tracking_number, shipperitemid=None):
-    xml = f'''<?xml version="1.0" encoding="utf-8" ?>
+    xml = f"""<?xml version="1.0" encoding="utf-8" ?>
                 <shippingApiRequest>
                     <mailItem>
                         <trackingNumber>{tracking_number}</trackingNumber>
                         <shipperItemId>{shipperitemid}</shipperItemId>
                     </mailItem>
                 </shippingApiRequest>
-            '''
+            """
     parameters = {
-        'apikey': apikey,
-        'command': 'cancelMailing',
-        'data': xml,
+        "apikey": apikey,
+        "command": "cancelMailing",
+        "data": xml,
     }
     r = requests.post(url, parameters)
     return r.content, r.status_code
 
 
-def create_bag(apikey, tracking_number, bag_id=None, comment=None,
-               shipper_item_id=None, label=0, label_format='PNG'):
-    xml = f'''<?xml version="1.0" encoding="utf-8" ?>
+def create_bag(
+    apikey,
+    tracking_number,
+    bag_id=None,
+    comment=None,
+    shipper_item_id=None,
+    label=0,
+    label_format="PNG",
+):
+    xml = f"""<?xml version="1.0" encoding="utf-8" ?>
                 <shippingApiRequest>
                     <bag>
                         <shipperBagId>{bag_id}</shipperBagId>
@@ -64,30 +75,30 @@ def create_bag(apikey, tracking_number, bag_id=None, comment=None,
                             <shipperItemId>{shipper_item_id}</shipperItemId>
                         </mailItem>
                     </bag>
-                </shippingApiRequest>'''
+                </shippingApiRequest>"""
     parameters = {
-        'apikey': apikey,
-        'command': 'createBag',
-        'data': xml,
-        'label': label,
-        'labelFormat': label_format,
+        "apikey": apikey,
+        "command": "createBag",
+        "data": xml,
+        "label": label,
+        "labelFormat": label_format,
     }
     r = requests.post(url, parameters)
     return r.content, r.status_code
 
 
 def cancel_bag(apikey, bag_id, shipper_bag_id):
-    xml = f'''<?xml version="1.0" encoding="utf-8" ?>
+    xml = f"""<?xml version="1.0" encoding="utf-8" ?>
                 <shippingApiRequest>
                     <bag>
                         <bagId>{bag_id}</bagId>
                         <shipperBagId>{shipper_bag_id}</shipperBagId>
                     </bag>
-                </shippingApiRequest>'''
+                </shippingApiRequest>"""
     parameters = {
-        'apikey': apikey,
-        'command': 'cancelBag',
-        'data': xml,
+        "apikey": apikey,
+        "command": "cancelBag",
+        "data": xml,
     }
     r = requests.post(url, parameters)
     return r.content, r.status_code
@@ -97,13 +108,3 @@ def create_manifest(apikey, csv, clearance_type):
 
     pass
 
-
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        fname = sg.popup_get_file('Please select consolidation file')
-    else:
-        fname = sys.argv[1]
-
-    test = create_mailing(fname, '8557d4855241ba2d',
-                          test_mode=1, label='Ground', label_format='PNG')
-    print(test)
